c6f6d8d8072b59073eb0a70a1dad05d57537a8f0
lisadelladucata <lisa.delladucata@gmail.com>
2024-11-28 16:17:45 +0100

    feat: add exercise

--- script.js
+++ script.js
@@ -5,6 +5,24 @@
 // - va applicato uno sconto del 40% per gli over 65.
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
-const (kilometres) = "quanti chilometri devi percorrere?"
-alert('quanti chilometri devi fare?')
-console.log(kilometres)
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+let kilometres = Number(prompt("quanti chilometri devi percorrere?"))
+let age = Number(prompt("quanti anni hai?"))
+const price= 0.21
+const priceUnderage = price - 0.05
+const priceOverage = 0.12
+// prezzo del biglietto in base ai kilometri
+if (kilometres >= 1 && age >= 18 && age < 65){
+    alert(price * kilometres +"€")
+} 
+// prezzo del biglietto se hai meno di 18anni 
+if (age < 18){
+    alert(priceUnderage * kilometres + "€")
+}
+
+// prezzo del biglietto se hai più di 65 anni 
+
+if (age > 65){
+    alert (priceOverage * kilometres + "€")
+}
